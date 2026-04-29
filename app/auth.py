c2617153aa3d5cde149ce776ab9db14392eac342
Romaric tatho <tathoromaric@gmail.com>
2026-04-29 14:44:18 +0100

    rectify deprecated functions on app/auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -32,10 +32,14 @@ def get_password_hash(password):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     """Génère le jeton JWT pour l'utilisateur"""
     to_encode = data.copy()
+
+    # On récupère l'heure actuelle "consciente" de l'UTC
+    now = datetime.now(timezone.utc) 
+
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = now + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = now + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
